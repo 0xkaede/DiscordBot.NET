f45b9024f9473bf24a381c34d64efb795553937b
kaede <69410676+0xkaede@users.noreply.github.com>
2021-09-10 08:03:03 +0100

    ServerInfo

--- DiscordBot/CommandsServices/GuildCommands.cs
+++ DiscordBot/CommandsServices/GuildCommands.cs
@@ -15,6 +15,8 @@ namespace DiscordBot.CommandsServices
         {
             SocketGuild guild = (SocketGuild)Context.Guild;
 
+            string uRoles = "";
+
             string[] guildinfo = { 
                 guild.Name, guild.IconUrl, 
                 guild.OwnerId.ToString(), 
@@ -28,25 +30,29 @@ namespace DiscordBot.CommandsServices
                 null
             };
 
-            if(Nitroinfo[0] == null || Nitroinfo[0] == "0")
-            {
+            string[] Channelsinfo = {
+                guild.CategoryChannels.Count.ToString(),
+                guild.TextChannels.Count.ToString(),
+                guild.VoiceChannels.Count.ToString(),
+            };
+
+
+            if (Nitroinfo[0] == null || Nitroinfo[0] == "0")
                 Nitroinfo[2] = "2";
-            }
-
             else if(Nitroinfo[0] == "1")
-            {
                 Nitroinfo[2] = "15";
-            }
             else if(Nitroinfo[0] == "2")
-            {
                 Nitroinfo[2] = "30";
-            }
             else if (Nitroinfo[0] == "3")
-            {
                 Nitroinfo[2] = guildinfo[3];
+
+            foreach (SocketRole role in guild.Roles)
+            {
+                if (!role.Name.ToLower().Contains("everyone"))
+                {
+                    uRoles += $"<@&{role.Id}> ".ToString();
+                }
             }
-
-
 
             var builder = new EmbedBuilder()
                     .WithAuthor(guildinfo[0], guildinfo[1])
@@ -56,6 +62,10 @@ namespace DiscordBot.CommandsServices
                     .AddField("Owner: ", $"<@{guildinfo[2]}>", true)
                     .AddField("Members: ", $"{guildinfo[3]}/{guildinfo[4]}", true)
                     .AddField("Boosts: ", $"Level {Nitroinfo[0]}\n{Nitroinfo[1]}/{Nitroinfo[2]} boosts", true)
+                    .AddField("Channel Categories: ", $" {Channelsinfo[0]}", true)
+                    .AddField("Text Channels: ", $" {guild.TextChannels.Count}", true)
+                    .AddField("Voice Channelss: ", $" {guild.VoiceChannels.Count}", true)
+                    .AddField($"Roles[{guild.Roles.Count}]: ", $"{uRoles}", true)
 
                     .WithFooter($"ID: {guild.Id.ToString()} | Server created: {guild.CreatedAt.ToString("MM/dd/yyyy")}")
                     .WithCurrentTimestamp();
